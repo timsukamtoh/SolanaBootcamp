f9ec2b50a67ac2d8c30de8f407aefc435982bdfa
Timothy Sukamtoh <timtoh168@gmail.com>
2024-04-03 16:35:05 -0700

    complete hw5

--- homeworks_rust/homeworks/homework5/if/if2.rs
+++ homeworks_rust/homeworks/homework5/if/if2.rs
@@ -4,16 +4,11 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
-
 pub fn fizz_if_foo(fizzish: &str) -> &str {
-    match fizzish == "fizz" {
-        "foo"
-    }
-    else if {
-        "fuzz"
-    } else {
-        "baz"
+    match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz"
     }
 }
 
